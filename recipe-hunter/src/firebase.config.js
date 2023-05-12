77f01aaf5115672c9d9373678cf18a58ffb06d72
senior9 <ovi215188@gmail.com>
2023-05-13 02:30:09 +0600

    enviroment variable

--- recipe-hunter/src/firebase.config.js
+++ recipe-hunter/src/firebase.config.js
@@ -5,13 +5,21 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBrWnMxIUCuPc3nhkrwTttFpZ_tkwZleJI",
-  authDomain: "recipe-hunter-86035.firebaseapp.com",
-  projectId: "recipe-hunter-86035",
-  storageBucket: "recipe-hunter-86035.appspot.com",
-  messagingSenderId: "776064576054",
-  appId: "1:776064576054:web:80fc66c4390ae7e8858697"
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId:  import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
 };
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBrWnMxIUCuPc3nhkrwTttFpZ_tkwZleJI",
+//   authDomain: "recipe-hunter-86035.firebaseapp.com",
+//   projectId: "recipe-hunter-86035",
+//   storageBucket: "recipe-hunter-86035.appspot.com",
+//   messagingSenderId: "776064576054",
+//   appId: "1:776064576054:web:80fc66c4390ae7e8858697"
+// };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
