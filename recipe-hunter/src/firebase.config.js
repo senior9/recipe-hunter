8d9bf35444f34963a5f4e8afe3664842e2a3b30e
senior9 <ovi215188@gmail.com>
2023-05-13 02:54:24 +0600

    all done

--- recipe-hunter/src/firebase.config.js
+++ recipe-hunter/src/firebase.config.js
@@ -12,14 +12,7 @@ const firebaseConfig = {
   messagingSenderId:  import.meta.env.VITE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_APP_ID,
 };
-// const firebaseConfig = {
-//   apiKey: "AIzaSyBrWnMxIUCuPc3nhkrwTttFpZ_tkwZleJI",
-//   authDomain: "recipe-hunter-86035.firebaseapp.com",
-//   projectId: "recipe-hunter-86035",
-//   storageBucket: "recipe-hunter-86035.appspot.com",
-//   messagingSenderId: "776064576054",
-//   appId: "1:776064576054:web:80fc66c4390ae7e8858697"
-// };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
